1c945909d1791cd9cddf92cef50d93b8ba8aea20
Truc Nguyen <trucn0215@gmail.com>
2020-10-28 17:58:48 -0700

    add array of quiz questions

--- JavaScript.js
+++ JavaScript.js
@@ -1,3 +1,82 @@
+// List of questions
+const questionsList = [
+    {
+        "question": "Inside which HTML element do we put the JavaScript?",
+        "answer": ["<javascript>", "<script>", "<js>", "<scripting>"],
+        "correct": 1
+    },
+    {
+        "question": "How do you write \"Hello World\" in an alert box?",
+        "answer": ["<jaalertBox(\"Hello World\")", "<msg(\"Hello World\");>", "msgBox(\"Hello World\");", "alert(\"Hello World\");  "],
+        "correct": 3
+    },
+    {
+        "question": "How to write an IF statement in JavaScript?",
+        "answer": ["if i == 5 then", "if (i == 5)", "if i = 5", "if i = 5 then"],
+        "correct": 1
+    },
+    {
+        "question": "How to write an IF statement for executing some code if \"i\" is NOT equal to 5?",
+        "answer": ["if i =! 5 then", "if i <> 5", "if (i <> 5)", "if (i != 5)  "],
+        "correct": 3
+    },
+    {
+        "question": "How does a FOR loop start?",
+        "answer": ["for i = 1 to 5", "for (i = 0; i <= 5)", "for (i = 0; i <= 5; i++)", "for (i <= 5; i++)"],
+        "correct": 2
+    },
+    {
+        "question": "How can you add a comment in a JavaScript?",
+        "answer": ["//", "'comment'", "'comment", "<!--comment-->"],
+        "correct": 0
+    },
+    {
+        "question": "How do you round the number 7.25, to the nearest integer?",
+        "answer": ["Math.rnd(7.25)", "round(7.25)", "Math.round(7.25)", "rnd(7.25)"],
+        "correct": 2
+    },
+    {
+        "question": "How do you find the number with the highest value of x and y?",
+        "answer": ["Math.ceil(x, y)", "top(x, y)", "Math.max(x, y)", "ceil(x, y)"],
+        "correct": 2
+    },
+    {
+        "question": "Which event occurs when the user clicks on an HTML element?",
+        "answer": ["onchange", "onmouseover", "onclick", "onmouseclick"],
+        "correct": 2
+    },
+    {
+        "question": "How do you declare a JavaScript variable?",
+        "answer": ["variable carName;", "var carName;", "v carName;", "None of above"],
+        "correct": 1
+    },
+    {
+        "question": "Which operator is used to assign a value to a variable?",
+        "answer": ["*", "=", "x", "=="],
+        "correct": 1
+    },
+    {
+        "question": "How does a WHILE loop start?",
+        "answer": ["while (i <= 10; i++)", "while (i <= 10)", "while i = 1 to 10", "None of above"],
+        "correct": 1
+    },
+    {
+        "question": "How do you call a function named \"myFunction\"?",
+        "answer": ["myFunction()", "call function myFunction()", "call myFunction()", "myFunction{}"],
+        "correct": 0
+    },
+    {
+        "question": "How do you create a function in JavaScript?",
+        "answer": ["function = myFunction()", "function:myFunction()", "function myFunction()", "function.myFunction()"],
+        "correct": 2
+    },
+    {
+        "question": "Where is the correct place to insert a JavaScript?",
+        "answer": ["The <body> section", "Both the <head> section and the <body> section are correct", "The <head> section", "The <main> section"],
+        "correct": 1
+    },
+]
+
 // Varible for Button functioning
 var startBtn = document.querySelector("#startBtn")
 var submitBtn = document.querySelector("#submit")
@@ -19,10 +98,9 @@ function startQuiz(){
 // function renderTime(){
 //     secondsDisplay.textContent = getFormattedSeconds();
 // }
-//console.log(startTimer);
 
 function startTimer(){
-    var totalTime = 10;
+    var totalTime = 75;
 
     var timeDisplace = document.getElementById("timer");
 
